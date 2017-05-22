51fd9841dc47bcbf8861a2028eb81b671552dcb6
Rebecca Way <rebeccaway2016@u.northwestern.edu>
2017-05-22 17:34:40 -0500

    master - I forgot to commit a bunch, but I have an html skeleton with a few placeholders, 5 example characters, and functions to move characters around and display them on screen.

--- assets/javascript/characters.js
+++ assets/javascript/characters.js
@@ -1,3 +1,34 @@
-var player_character = {
+var character_one = {
+  hp: 35,
+  attack: 5,
+  counter: 4,
+  name: "char 1"
+};
 
+var character_two = {
+  hp: 40,
+  attack: 4,
+  counter: 2,
+  name: "char 2"
+};
+
+var character_three = {
+  hp: 60,
+  attack: 2,
+  counter: 3,
+  name: "char 3"
+};
+
+var character_four = {
+  hp: 50,
+  attack: 3,
+  counter: 1,
+  name: "char 4"
+};
+
+var character_five = {
+  hp: 25,
+  attack: 7,
+  counter: 8,
+  name: "char 5"
 };
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,83 @@
 var game = {
-  player_character = null;
-  opponent = null;
+  player_character: null,
+  opponent_character: null,
+  available_characters: [character_one, character_two, character_three, character_four, character_five],
+
+  setPlayerCharacter: function(character){
+    if(isInArray(character, game.available_characters)){
+      game.player_character = character;
+      removeItemFromArray(character, this.available_characters);
+      $('#player_character').text(character.hp);
+      game.moveCharacter(character, "attacker");
+      game.updateCharacterDisplay();
+    }
+  },
+
+  setOpponentCharacter: function(character){
+    if(isInArray(character, game.available_characters)){
+      game.opponent_character = character;
+      removeItemFromArray(character, this.available_characters);
+      $('#player_character').text(character.hp);
+      game.moveCharacter(character, "defender");
+    }
+  },
+
+  updateCharacterDisplay: function(){
+    //add available characters to top
+    var allcharacters = $(".charholder");
+    var characterspace;
+    allcharacters.empty();
+    for(var i=0;i<game.available_characters.length;i++){
+      characterspace = game.createCharacterStats(game.available_characters[i]);
+      allcharacters.append(characterspace);
+    }
+    //add attacker
+    var characterstats;
+    if(this.player_character){
+      moveCharacter(this.player_character, "attacker");
+    }
+    //add defender
+    if(opponent_character){
+      moveCharacter(this.opponent_character, "defender");
+    }
+  },
+
+  createCharacterStats:function(character){
+    charactername = character.name;
+    characterhp = character.hp;
+    characterattack = character.attack;
+    return '<div class="characterspace">' +
+                  '<p class="charactertext charactername">'+charactername+'</p>' +
+                  '<p class="charactertext characterhp">'+characterhp+'</p>' +
+                  '<p class="charactertext characterattack">'+characterattack+'</p>' +
+               '</div>';
+  },
+
+  resetCharacters: function(){
+    //make all characters available again
+    game.available_characters = [character_one, character_two, character_three, character_four, character_five];
+    $("#attacker").empty();
+    $("#defender").empty();
+    game.updateCharacterDisplay();
+  },
+
+  moveCharacter: function(character){
+    //move a character to the attacking or defending position
+    var characterstats = game.createCharacterStats(character);
+    $("#"+position).append(characterstats);
+  }
 };
+
+
+function removeItemFromArray(item, array){
+    return array.splice($.inArray(item, array),1);
+}
+
+function isInArray(item, array){
+  if(array.indexOf(item)>=0){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
