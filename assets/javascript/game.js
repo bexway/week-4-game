c2b7c694e16a2b9b940dbc23984ee3cfa749f241
Rebecca Way <rebeccaway2016@u.northwestern.edu>
2017-05-25 19:03:58 -0500

    master - game is functional, general messages added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,21 +2,16 @@ var game = {
   attacker_character: null,
   defender_character: null,
   available_characters: [character_one, character_two, character_three, character_four, character_five],
-  gamestate: 0,
-  //0 is waiting for attacker choice, 1 is waiting for opponent choice, 2 is battle
 
   setAttacker: function(character){
-    // if(this.gamestate<1){
       if(isInArray(character, game.available_characters)&&!game.attacker_character){
         game.attacker_character = character;
         removeItemFromArray(character, this.available_characters);
-        // $('#attacker_character').text(character.hp);
         game.updateFighter(character, "attacker");
         game.updateCharacterDisplay();
       }
       else{
         console.log("error");
-      // }
     }
   },
 
@@ -24,7 +19,6 @@ var game = {
     if(isInArray(character, game.available_characters)&&!game.defender_character){
       game.defender_character = character;
       removeItemFromArray(character, this.available_characters);
-      // $('#defender_character').text(character.hp);
       game.updateFighter(character, "defender");
       game.updateCharacterDisplay();
     }
@@ -42,7 +36,6 @@ var game = {
       characterstatblock = game.createCharacterStats(game.available_characters[i]);
       characterstatblock.addClass("available");
       allcharacters.append(characterstatblock);
-      // console.log(characterstatblock.data());
     }
   },
 
@@ -51,12 +44,6 @@ var game = {
     var charactermaxhp = character.maxhp;
     var charactercurrenthp = character.currenthp;
     var characterattack = character.currentattack;
-    // return '<div class="characterstatblock" id="'+charactername+'">' +
-    //               '<p class="charactertext charactername">'+charactername+'</p>' +
-    //               '<p class="charactertext charactermaxhp">'+charactermaxhp+'</p>' +
-    //               '<p class="charactertext charactercurrenthp">'+charactercurrenthp+'</p>' +
-    //               '<p class="charactertext characterattack">'+characterattack+'</p>' +
-    //            '</div>';
     var jqcharacter = $('<div>').addClass("characterstatblock").attr("id", charactername).data("charactervar", character);
     var stats = '<p class="charactertext charactername">'+charactername+'</p>' +
     '<p class="charactertext charactermaxhp">'+charactermaxhp+'</p>' +
@@ -89,30 +76,11 @@ var game = {
     $("#"+position).html(characterstats);
   },
 
-  // takeTurn: function(){
-  //   //Run this when the attack button is clicked
-  //   //First, the attacker makes their move
-  //   this.makeAttack();
-  //   //if the defender is dead, remove them from play and check if the game is over. They don't counterattack since they're dead
-  //   if(this.checkIfDead(this.defender_character)){
-  //     this.removeDefender();
-  //   }
-  //   //Otherwise, make a counterattack, and check if the attacker is dead
-  //   else{
-  //     this.counterAttack();
-  //     if(this.checkIfDead(this.attacker_character)){
-  //       console.log("test");
-  //     }
-  //   }
-  // },
   takeTurn: function(){
-    //Run this when the attack button is clicked
     //First, the attacker makes their move
-    console.log("Attack");
     this.makeAttack();
     //if the defender is dead, remove them from play and check if the game is over. They don't counterattack since they're dead
     if(!this.checkIfDead(this.defender_character)){
-      console.log("counter");
       this.counterAttack();
     }
   },
@@ -127,7 +95,7 @@ var game = {
 
   counterAttack: function(){
     this.attacker_character.currenthp -= this.defender_character.counter;
-    this.updateFighter(this.attacker_character, "attackers");
+    this.updateFighter(this.attacker_character, "attacker");
     this.updateFighter(this.defender_character, "defender");
   },
 
@@ -171,6 +139,64 @@ var game = {
 };
 
 
+
+
+
+
+//since the character stat blocks are dynamic, the onclick is set up attached to the document
+$(document).on('click', '.available', function(){
+  //set attacker or defender, whichever needs to be set
+    if(!game.attacker_character){
+      game.setAttacker($(this).data("charactervar"));
+      $('#genmessage').text("Choose your opponent!");
+    }
+    else if(!game.defender_character){
+      game.setDefender($(this).data("charactervar"));
+      $('#genmessage').text("Click the Attack button below to FIGHT!!");
+      $('.attackbtn').toggle();
+    }
+  });
+
+$('.attackbtn').click(function(){
+  game.takeTurn();
+  if(game.checkIfDead(game.attacker_character)){
+    $('.attackbtn').toggle();
+    game.removeAttacker();
+    $('#genmessage').text("Oh no, you lost! Click the Reset button below to try again!");
+  }
+  else if(game.checkIfDead(game.defender_character)){
+    $('.attackbtn').toggle();
+    game.removeDefender();
+    $('#genmessage').text("You won that battle, but there's more to fight! Choose another opponent!");
+  }
+  if(game.checkIfGameOver()){
+    $('.resetbtn').toggle();
+    if(game.attacker_character){
+      $('#genmessage').text("You did it, you're the best! Click the Reset button below to play again!");
+    }
+  }
+});
+
+$('.resetbtn').click(function(){
+  game.startGame();
+  $('.resetbtn').toggle();
+  $('#genmessage').text("Choose your character!");
+});
+
+
+
+
+
+game.startGame();
+
+
+
+
+
+
+
+
+
 function removeItemFromArray(item, array){
     return array.splice($.inArray(item, array),1);
 }
@@ -183,42 +209,3 @@ function isInArray(item, array){
     return false;
   }
 }
-
-//since the character stat blocks are dynamic, the onclick is set up attached to the document
-$(document).on('click', '.available', function(){
-  //set attacker or defender, whichever needs to be set
-    if(!game.attacker_character){
-      game.setAttacker($(this).data("charactervar"));
-    }
-    else if(!game.defender_character){
-      game.setDefender($(this).data("charactervar"));
-      $('.attackbtn').toggle();
-    }
-  });
-
-$('.attackbtn').click(function(){
-  game.takeTurn();
-  if(game.checkIfDead(game.attacker_character)){
-    $('.attackbtn').toggle();
-    game.removeAttacker();
-  }
-  else if(game.checkIfDead(game.defender_character)){
-    $('.attackbtn').toggle();
-    game.removeDefender();
-  }
-  if(game.checkIfGameOver()){
-    $('.resetbtn').toggle();
-  }
-});
-
-$('.resetbtn').click(function(){
-  game.startGame();
-  $('.resetbtn').toggle();
-});
-
-
-//Below: TESTING SETUP code
-game.startGame();
-
-// game.setAttacker(character_two);
-// game.setDefender(character_five);
